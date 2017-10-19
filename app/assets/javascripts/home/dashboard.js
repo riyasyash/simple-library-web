e328c55d48e4e478014de70155509ddcc7485ad2
riyasp <riyasp@sahajsoft.com>
2017-10-19 12:10:30 +0530

    using button_to instead of buttons and removing js for dashboard

--- app/assets/javascripts/home/dashboard.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('#home').click(function(){
-    $(location).attr('href','index');
-  });
-
-  $('#listBooks').click(function(){
-    $(location).attr('href','books');
-  });
-});
