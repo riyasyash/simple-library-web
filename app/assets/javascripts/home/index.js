6f436641444aa8dd0e174e83c5647c0f1723b469
riyasp <riyasp@sahajsoft.com>
2017-10-19 11:11:12 +0530

    added toster for book added notifications

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -1,35 +1,49 @@
 $(document).ready(function(){
   $("#saveBook").click(function(){
     $('#addBookForm').hide();
-    $('#loading').show();
-    $.ajax({
-      type: "POST",
-      url: "/books/create",
-      data: {"isbn":$("#isbn").val(),
-              "count":$("#bookCount").val()
-      },
-      success: function(data){
-        $('#loading').hide();
-        $('#modalBody').hide();
-        console.log(data);
-        var data_hash = data;
-        $('#bookThumbnail').attr("src",data_hash.small_thumbnail);
-        document.getElementById('book_title').innerHTML=data_hash.title;
-        document.getElementById('totalCount').innerHTML='Total :'+data_hash.total_count;
-        document.getElementById('availableCount').innerHTML='Avalilable :<font color=green>'+data_hash.available+'</font>';
-        $('#bookDetails').show();
-        // alert('success');
-      },
-      error: function(data) {
-        $('#loading').hide();
-        $('#addBookModal').hide();
-        alert('failure');
-      }
-    });
-    return false;
+    if($("#isbn").val()!=""){
+      $('#loading').show();
+      $.ajax({
+        type: "POST",
+        url: "/books/create",
+        data: {"isbn":$("#isbn").val(),
+                "count":$("#bookCount").val()
+        },
+        success: function(data){
+          toastr.success('Book Added');
+          $('#loading').hide();
+          $('#modalBody').hide();
+          console.log(data);
+          var data_hash = data;
+          $('#bookThumbnail').attr("src",data_hash.small_thumbnail);
+          document.getElementById('book_title').innerHTML=data_hash.title;
+          document.getElementById('totalCount').innerHTML='Total :'+data_hash.total_count;
+          document.getElementById('availableCount').innerHTML='Avalilable :<font color=green>'+data_hash.available+'</font>';
+          $('#bookDetails').show();
+          // alert('success');
+        },
+        error: function(data) {
+          $('#loading').hide();
+          $('#addBookModal').hide();
+          alert('failure');
+        }
+      });
+      return false;
+    }
+    else{
+      $('#loading').hide();
+      $('#addBookModal').hide();
+      toastr.error('Incomplete input');
+    }
   });
 
   $('#dashboard').click(function(){
     $(location).attr('href','dashboard');
-  })
+  });
+
+  $('#closeModal').click(function(){
+    $("#addBookModal").removeData('bs.modal');
+  });
+
+
 });
